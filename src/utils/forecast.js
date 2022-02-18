c009ac835be32ef1cc99f4a085b4e6460787d27f
Sachin Olla <sachin.olla@affle.com>
2022-02-19 00:12:44 +0530

    Added Wind conditions

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,11 @@ const forecast = (lati, long, callback) => {
             callback('Error: Unable to weather of the given location', undefined);
             return;
         }
-        const outputString = `Address: ${response.body.location.name}, ${response.body.location.region}, ${response.body.location.country}\n${response.body.location.localtime}\t Temp: ${response.body.current.temperature}°C\t ${response.body.current.weather_descriptions[0]}`;
+        const outputString = `
+Address: ${response.body.location.name}, ${response.body.location.region}, ${response.body.location.country}\n
+${response.body.location.localtime}\t Temp: ${response.body.current.temperature}°C\t ${response.body.current.weather_descriptions[0]}\n
+Wind: ${response.body.current.wind_speed} km/hr ${response.body.current.wind_degree}°${response.body.current.wind_dir}
+            `;
         callback(undefined, outputString);
     })
 }
